13c376b96212c982faa18c81811ecf8172445480
Natalie Sturm <ge94say@mytum.de>
2026-06-15 22:34:16 +0200

    new finetuning, save model after each epoch

--- finetuning/finetune.py
+++ finetuning/finetune.py
@@ -156,6 +156,7 @@ training_args = SFTConfig(
     lr_scheduler_type="cosine",
     eval_strategy="epoch",
     save_strategy="epoch",
+    save_total_limit=8,
     load_best_model_at_end=True,
     metric_for_best_model="eval_loss",
     greater_is_better=False,
